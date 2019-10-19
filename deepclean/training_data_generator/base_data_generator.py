70453f04737c1b6a98ab24588f6caa00dab49014
Pavel Kral <pavel.kral@rossum.ai>
2019-10-19 13:00:14 +0200

    Add BaseDataGenerator class

---
+++ deepclean/training_data_generator/base_data_generator.py
@@ -0,0 +1,27 @@
+import keras
+from deepclean import settings
+
+
+class BaseDataGenerator(keras.utils.Sequence):
+    def __init__(self, validation=False):
+        self.validation = validation
+        self.batch_data = []
+
+        self.data_dir = settings.PREPARED_VALIDATION_DATA_DIR if validation else settings.PREPARED_TRAINING_DATA_DIR
+
+        file_ids = self.get_file_ids()
+        self.prepare_batches(file_ids)
+
+    def __len__(self):
+        return len(self.batch_data)
+
+    def get_file_ids(self):
+        images_dir = self.data_dir / "graffiti"
+        all_images = images_dir.glob("*.jpg")
+        return [image.name.split(".")[0] for image in all_images]
+
+    def prepare_batch(self, batch):
+        raise NotImplementedError()
+
+    def prepare_batches(self, file_ids):
+        raise NotImplementedError()
