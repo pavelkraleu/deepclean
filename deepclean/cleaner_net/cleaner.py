c51501ac1806235efebb79899b2b8d074692d313
Pavel Kral <pavel.kral@rossum.ai>
2019-10-19 13:01:53 +0200

    Add Cleaner class

--- deepclean/cleaner_net/cleaner.py
+++ deepclean/cleaner_net/cleaner.py
@@ -1,5 +1,4 @@
 import glob
-
 import cv2
 import imageio
 from segmentation_models.backbones import get_preprocessing
@@ -7,54 +6,75 @@ import numpy as np
 from deepclean import settings
 from deepclean.remover_net.network import RemoverNeuralNetwork
 from deepclean.detector_net.network import SegmentationNeuralNetwork
-from deepclean.remover_net.remover_sequence_generator import RemoverSequenceGenerator
-from deepclean.tools import debug_data_generator
 import os
 import matplotlib.pyplot as plt
 
 
 class Cleaner:
-    video_files = glob.glob('./video_files/*.MOV')
+    video_files = glob.glob(settings.CLEANER_VIDEO_FILES_PATH)
 
     video_resolution = (1920, 1088)
-
-    remvover_sequence_generator = RemoverSequenceGenerator()
 
     def __init__(self):
 
         self.segmentation_net = SegmentationNeuralNetwork()
         self.remover_net = RemoverNeuralNetwork()
 
-    def process_frame(self, original_frame):
+    def process_data(self, with_graffiti, masks, backgrounds):
 
-        # frame = cv2.imread(frame_path)
+        final_masks = []
+        final_backgrounds = []
+        final_backgrounds_orig = []
+
+        for i in range(masks.shape[0]):
+
+            mask = masks[i]
+            background = backgrounds[i]
+            background_orig = np.array(backgrounds[i])
+
+            kernel = np.ones((5, 5), np.uint8)
+            mask = cv2.dilate(mask, kernel, iterations=1)
+
+            mask = mask[..., np.newaxis]
+            # print(mask.shape)
+
+            mask = np.repeat(mask, 3, axis=2).astype(int)
+            background[mask == 255] = 255
+
+            final_masks.append(mask)
+            final_backgrounds.append(background)
+            final_backgrounds_orig.append(background_orig)
+
+            # output_data = mask / 255
+
+        return ({
+            "inputs_img": np.stack(final_backgrounds) / 255,
+            'inputs_mask': 1 - (np.stack(final_masks) / 255)
+        }, {
+            "outputs_img": np.stack(final_backgrounds_orig) / 255
+        })
+
+    def process_frame(self, original_frame):
         frame = original_frame.copy()
         frame = cv2.resize(frame, settings.DETECTOR_NETWORK_RESOLUTION)
-
-        cv2.imwrite("frame.png", frame)
 
         segmentation = get_preprocessing(
             settings.DETECTOR_NETWORK_BACKEND)(frame)
 
-        segmentation = self.segmentation_net.model.predict([[segmentation]])[0]
+        segmentation_orig = self.segmentation_net.model.predict(
+            [[segmentation]])[0]
 
-        # cv2.imwrite("segmentation.png", segmentation)
+        ret, segmentation = cv2.threshold(segmentation_orig,
+                                          settings.GENERATE_OUTPUT_THRESHOLD,
+                                          1.0, cv2.THRESH_BINARY)
 
-        ret, segmentation = cv2.threshold(segmentation, 0.5, 1.0,
-                                          cv2.THRESH_BINARY)
+        kernel = np.ones((2, 2), np.uint8)
+        opening = cv2.morphologyEx(segmentation, cv2.MORPH_OPEN, kernel)
+        opening = cv2.dilate(opening, np.ones((3, 3)))
 
-        # segmentation = 1 - segmentation
-        #
-        # print(segmentation.min())
-        # print(segmentation.max())
-
-        # cv2.imwrite("segmentation-a.png", segmentation * 255)
-
-        data_to_remove = self.remvover_sequence_generator.process_data(
-            np.array([frame]), np.array([segmentation * 255]),
-            np.array([frame]))
-
-        # debug_data_generator([data_to_remove])
+        data_to_remove = self.process_data(np.array([frame]),
+                                           np.array([opening * 255]),
+                                           np.array([frame]))
 
         removed_frame = self.remover_net.model.predict(data_to_remove[0])[0]
 
@@ -68,15 +88,16 @@ class Cleaner:
         frame = cv2.resize(frame, settings.DETECTOR_NETWORK_RESOLUTION)
         frame[mask] = removed_frame[mask]
 
-        cv2.imwrite("frame.png", frame)
+        return frame, opening
 
-        return frame
-
-    def save_sample(self, x, y, output_path):
+    def save_sample(self, x, y, mask, output_path):
 
         fig, axs = plt.subplots(1, 2, figsize=(16, 9), dpi=150)
         axs[0].imshow(x)
         axs[1].imshow(y)
+
+        axs[0].set_title(f"Source frame", fontdict={'fontsize': 20})
+        axs[1].set_title(f"Processed frame", fontdict={'fontsize': 20})
 
         for i in [0, 1]:
             axs[i].tick_params(left=False, bottom=False)
@@ -87,14 +108,26 @@ class Cleaner:
         plt.savefig(output_path)
         plt.close(fig)
 
+        frame = cv2.imread(output_path)
+        mask = cv2.cvtColor(mask, cv2.COLOR_GRAY2RGB) * 255
+        mask = cv2.resize(mask, (400, 400))
+
+        x_offset = int((frame.shape[1] - mask.shape[1]) / 2)
+        y_offset = frame.shape[0] - mask.shape[0] - 50
+        frame[y_offset:y_offset + mask.shape[0], x_offset:x_offset +
+              mask.shape[1]] = mask
+
+        cv2.imwrite(output_path, frame)
+
     def process_video(self):
         for video_file in self.video_files:
             print(video_file)
             output_video_file = f'./training_results/clean_{os.path.basename(video_file)}.mp4'
+            capture = cv2.VideoCapture(video_file)
+            fps = int(capture.get(cv2.CAP_PROP_FPS))
             with imageio.get_writer(output_video_file, mode='?',
-                                    fps=30) as writer:
+                                    fps=fps) as writer:
 
-                capture = cv2.VideoCapture(video_file)
                 while (capture.isOpened()):
                     ret, frame = capture.read()
 
@@ -106,15 +139,12 @@ class Cleaner:
                                    (settings.DETECTOR_NETWORK_RESOLUTION)),
                         cv2.COLOR_BGR2RGB)
 
-                    result = self.process_frame(frame_orig)
+                    result, segmentation = self.process_frame(frame_orig)
 
-                    self.save_sample(frame_orig, result,
-                                     f'./training_results/tmp.png')
+                    self.save_sample(frame_orig, result, segmentation,
+                                     f'./ramdisk/tmp.png')
 
-                    # dddd
-
-                    img_file = cv2.cvtColor(
-                        cv2.imread(f'./training_results/tmp.png'),
-                        cv2.COLOR_BGR2RGB)
+                    img_file = cv2.cvtColor(cv2.imread(f'./ramdisk/tmp.png'),
+                                            cv2.COLOR_BGR2RGB)
                     img_file = cv2.resize(img_file, self.video_resolution)
                     writer.append_data(img_file)
